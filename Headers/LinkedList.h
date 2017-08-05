6f49fc2290046d9b13cf676f9cbd551db00a6383
Antonio Castro <antoniocastro@Antonios-MBP.attlocal.net>
2017-08-05 10:52:38 -0700

    This is the first commit. added LinkedList.cpp and LinkedList.h

---
+++ Headers/LinkedList.h
@@ -0,0 +1,36 @@
+//
+// Created by Antonio Castro on 8/5/17.
+//
+
+#ifndef LINKEDLIST_LINKEDLIST_H
+#define LINKEDLIST_LINKEDLIST_H
+
+
+class LinkedList {
+
+private:
+
+    struct Node{
+        int data;
+        Node* next;
+    };
+
+    Node* head;
+    Node* tail;
+    Node* temp;
+
+public:
+
+    /*constructor*/
+    LinkedList();
+    void addNode(int data);
+    void deleteNode(int index);
+    void printList();
+    void reverseList();
+    void addNodeSorted();
+
+
+};
+
+
+#endif //LINKEDLIST_LINKEDLIST_H
---
+++ Source/LinkedList.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Antonio Castro on 8/5/17.
+//
+
+#include <cstdlib>
+#include <iostream>
+
+#include "../Headers/LinkedList.h"
+
+using namespace std;
+
+LinkedList::LinkedList() {
+    head = NULL;
+    tail = NULL;
+    temp = NULL;
+
+}
+
+//This will add a node to the end of the linked list
+void LinkedList::addNode(int data) {
+    Node* newNode = new Node;
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if(head != NULL) {
+        tail->next = newNode;
+        tail = newNode;
+    }
+    else{
+        head = newNode;
+        tail = newNode;
+    }
+}
+
+void LinkedList::deleteNode(int index) {
+
+}
+
+void LinkedList::printList() {
+    temp = head;
+
+
+}
+
+void LinkedList::addNodeSorted() {
+
+}
+
+void LinkedList::reverseList() {
+
+}
